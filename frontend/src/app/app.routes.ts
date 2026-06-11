f79ef4fd76eb5212f5588fc842d9147460d8a33a
Andraž Dobnikar <andraz.dobnikar@hotmail.com>
2026-06-11 02:03:58 +0200

    feat(frontend): add contact routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'contacts', pathMatch: 'full' },
+  {
+    path: 'contacts',
+    loadComponent: () =>
+      import('./features/contacts/contact-list/contact-list.component').then((m) => m.ContactListComponent),
+  },
+  {
+    path: 'contacts/new',
+    loadComponent: () =>
+      import('./features/contacts/contact-form/contact-form.component').then((m) => m.ContactFormComponent),
+  },
+  {
+    path: 'contacts/:id',
+    loadComponent: () =>
+      import('./features/contacts/contact-detail/contact-detail.component').then((m) => m.ContactDetailComponent),
+  },
+  {
+    path: 'contacts/:id/edit',
+    loadComponent: () =>
+      import('./features/contacts/contact-form/contact-form.component').then((m) => m.ContactFormComponent),
+  },
+  { path: '**', redirectTo: 'contacts' },
+];
